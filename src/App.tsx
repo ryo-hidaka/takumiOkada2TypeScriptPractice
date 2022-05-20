d467023f3fb144dcd2016603ada00d2ede6ff7e2
ryo-hidaka <66785120+ryo-hidaka@users.noreply.github.com>
2022-05-20 19:06:56 +0900

    typescript-reactの途中経過

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,34 @@ import { useState } from "react";
 import { Todo } from "./Todo";
 import "./styles.css";
 
+type TodoType = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
+
 export default function App() {
-  const [todos, setTodos] = useState<any>([]);
+  const [todos, setTodos] = useState<Array<TodoType>>([]);
   const onCilckFetchData = () => {
-    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
-      setTodos(res.data);
-    });
+    axios
+      .get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => {
+        setTodos(res.data);
+      });
   };
   return (
     <div className="App">
       <button onClick={onCilckFetchData}>データ取得</button>
       {todos.map((todo) => {
-        return <Todo title={todo.title} userid={todo.userId} />;
+        return (
+          <Todo
+            key={todo.id}
+            title={todo.title}
+            userId={todo.userId}
+            completed={todo.completed}
+          />
+        );
       })}
     </div>
   );
